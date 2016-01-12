a21309cbfd2c1ab22e7ee9ceda56641a6adb04e6
David Pronk <git@davidpronk.nl>
2016-01-12 19:22:48 +0100

    Update mrktplts.user.js
    Removed dependency for jQuery. Vanilla all the way...

--- mrktplts.user.js
+++ mrktplts.user.js
@@ -4,21 +4,67 @@
 // @description Remove commercial offers from Marktplaats search results
 // @include     http://www.marktplaats.nl/*
 // @grant       none
-// @version     0.1
+// @version     0.2
 // ==/UserScript==
 
-if ( typeof jQuery !== 'undefined' ) {
+var locations = document.querySelectorAll( '.location-name' );
 
-    var locs = $( '.location-name' );
+for( var x=0 ; x< locations.length ; x++ ){
 
-    for( var x=0 ; x< locs.length ; x++ ){
+    var node = locations[x];
+    var nodeContent = node.innerText;
 
-        var node = locs[x],
-            nodeContent = $( node ).text();
-
-        if( nodeContent.indexOf( 'Bezorgt' ) !== -1 ){
-            $( node ).parents( '.search-result' ).remove();
+    if( nodeContent.indexOf( 'Bezorgt' ) !== -1 ){
+               
+        var parents = getParents( node, '.search-result' );
+        
+        for( var y=0 ; y< parents.length ; y++ ){
+            var parent = parents[y];
+            parent.parentNode.removeChild( parent );
         }
-
+        
     }
+
 }
+
+
+
+// Based on a script from http://gomakethings.com/climbing-up-and-down-the-dom-tree-with-vanilla-javascript/
+
+/**
+ * Get all DOM element up the tree that contain a class
+ * @param  {Node} elem The base element
+ * @param  {String} selector The class, id, data attribute, or tag to look for
+ * @return {Array} Null if no match
+ */
+function getParents(elem, selector) {
+
+    var parents = [];
+    if ( selector ) {
+        var firstChar = selector.charAt(0);
+    }
+
+    // Get matches
+    for ( ; elem && elem !== document; elem = elem.parentNode ) {
+        if ( selector ) {
+
+            // If selector is a class
+            if ( firstChar === '.' ) {
+                if ( elem.classList.contains( selector.substr(1) ) ) {
+                    parents.push( elem );
+                }
+            }
+
+        } else {
+            parents.push( elem );
+        }
+    }
+
+    // Return parents if any exist
+    if ( parents.length === 0 ) {
+        return null;
+    } else {
+        return parents;
+    }
+
+};
