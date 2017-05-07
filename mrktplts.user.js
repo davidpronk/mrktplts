25dd0da56ef9a6747c8e8a79d96fa87a35bd9c44
Pronkert <git@davidpronk.nl>
2017-05-07 14:14:28 +0200

    Self execute

--- mrktplts.user.js
+++ mrktplts.user.js
@@ -7,64 +7,69 @@
 // @version     0.2
 // ==/UserScript==
 
-var locations = document.querySelectorAll( '.location-name' );
+(function() {
+    'use strict';
 
-for( var x=0 ; x< locations.length ; x++ ){
+        var locations = document.querySelectorAll( '.location-name' );
 
-    var node = locations[x];
-    var nodeContent = node.innerText;
+        for( var x=0 ; x< locations.length ; x++ ){
 
-    if( nodeContent.indexOf( 'Bezorgt' ) !== -1 ){
-               
-        var parents = getParents( node, '.search-result' );
-        
-        for( var y=0 ; y< parents.length ; y++ ){
-            var parent = parents[y];
-            parent.parentNode.removeChild( parent );
+            var node = locations[x];
+            var nodeContent = node.innerText;
+
+            if( nodeContent.indexOf( 'Bezorgt' ) !== -1 ){
+
+                var parents = getParents( node, '.search-result' );
+
+                for( var y=0 ; y< parents.length ; y++ ){
+                    var parent = parents[y];
+                    parent.parentNode.removeChild( parent );
+                }
+
+            }
+
         }
-        
-    }
-
-}
 
 
 
-// Based on a script from http://gomakethings.com/climbing-up-and-down-the-dom-tree-with-vanilla-javascript/
+        // Based on a script from http://gomakethings.com/climbing-up-and-down-the-dom-tree-with-vanilla-javascript/
 
-/**
- * Get all DOM element up the tree that contain a class
- * @param  {Node} elem The base element
- * @param  {String} selector The class, id, data attribute, or tag to look for
- * @return {Array} Null if no match
- */
-function getParents(elem, selector) {
+        /**
+         * Get all DOM element up the tree that contain a class
+         * @param  {Node} elem The base element
+         * @param  {String} selector The class, id, data attribute, or tag to look for
+         * @return {Array} Null if no match
+         */
+        function getParents(elem, selector) {
 
-    var parents = [];
-    if ( selector ) {
-        var firstChar = selector.charAt(0);
-    }
+            var parents = [];
+            if ( selector ) {
+                var firstChar = selector.charAt(0);
+            }
 
-    // Get matches
-    for ( ; elem && elem !== document; elem = elem.parentNode ) {
-        if ( selector ) {
+            // Get matches
+            for ( ; elem && elem !== document; elem = elem.parentNode ) {
+                if ( selector ) {
 
-            // If selector is a class
-            if ( firstChar === '.' ) {
-                if ( elem.classList.contains( selector.substr(1) ) ) {
+                    // If selector is a class
+                    if ( firstChar === '.' ) {
+                        if ( elem.classList.contains( selector.substr(1) ) ) {
+                            parents.push( elem );
+                        }
+                    }
+
+                } else {
                     parents.push( elem );
                 }
             }
 
-        } else {
-            parents.push( elem );
-        }
-    }
+            // Return parents if any exist
+            if ( parents.length === 0 ) {
+                return null;
+            } else {
+                return parents;
+            }
 
-    // Return parents if any exist
-    if ( parents.length === 0 ) {
-        return null;
-    } else {
-        return parents;
-    }
+        };
 
-};
+})();
